15309895bf3a1ffed0bf5b4cbca62c62305dbbb6
Luna Duclos <luna.duclos@embark-studios.com>
2020-12-03 13:38:43 +0100

    Use a more generic way of figuring out what to call on linux

--- browser_linux.go
+++ browser_linux.go
@@ -3,15 +3,18 @@ package browser
 import "os/exec"
 
 func openBrowser(url string) error {
-	// Unfortunately, due to how WSL works, we can't differentiate between WSL and Linux via build tags
-	// So we try xdg-open first, and if that fails, use wslview
-	if err := runCmd("xdg-open", url); err != nil {
-		if wslErr := runCmd("wslview", url); wslErr != nil {
-			return err
+	providers := []string{"xdg-open", "x-www-browser", "www-browser"}
+
+	// There are multiple possible providers to open a browser on linux
+	// One of them is xdg-open, another is x-www-browser, then there's www-browser, etc.
+	// Look for one that exists and run it
+	for _, provider := range providers {
+		if _, err := exec.LookPath(provider); err == nil {
+			return runCmd(provider, url)
 		}
 	}
 
-	return nil
+	return exec.ErrNotFound
 }
 
 func setFlags(cmd *exec.Cmd) {}
