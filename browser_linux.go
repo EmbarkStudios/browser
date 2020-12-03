eb5c56a4c1a93ead8a8e2ac4826b42d1db56382d
Luna Duclos <luna.duclos@embark-studios.com>
2020-12-03 11:29:29 +0100

    Add WSL support via wslview

--- browser_linux.go
+++ browser_linux.go
@@ -3,7 +3,15 @@ package browser
 import "os/exec"
 
 func openBrowser(url string) error {
-	return runCmd("xdg-open", url)
+	// Unfortunately, due to how WSL works, we can't differentiate between WSL and Linux via build tags
+	// So we try xdg-open first, and if that fails, use wslview
+	if err := runCmd("xdg-open", url); err != nil {
+		if wslErr := runCmd("wslview", url); wslErr != nil {
+			return err
+		}
+
+		return nil
+	}
 }
 
 func setFlags(cmd *exec.Cmd) {}
